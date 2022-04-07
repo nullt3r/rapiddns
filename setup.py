704451d87078051bf0a7b8642c1703cd4dd8cd5d
nullt3r <nullt3r@bugdelivery.com>
2022-04-07 20:53:00 +0200

    updated setupy.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "Programming Language :: Python :: 3.9",
     ],
     packages=find_packages(),
-    install_requires=["requests", "bs4"],
+    install_requires=["requests", "bs4", "logging"],
     entry_points={
         "console_scripts": [
             "rapiddns=rapiddns.__main__:main",
