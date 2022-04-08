048cd63f5df152ae5f6c9e6fc200018ed71db174
nullt3r <nullt3r@bugdelivery.com>
2022-04-08 10:14:00 +0200

    fixed endpoint mistake

--- rapiddns/__main__.py
+++ rapiddns/__main__.py
@@ -12,7 +12,6 @@ class RapidDns:
         try:
             request = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"})
             html = request.content
-            print(request.request.url)
         except Exception as e:
             logging.error(
                 "there was an error while reaching rapiddns.io: %s", e
